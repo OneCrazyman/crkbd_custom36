bd7145f45519c52354868657394e97365746106e
OneCrazyman <cjw7155@naver.com>
2024-06-02 06:23:34 +0900

    #

--- keymaps/custom36/keymap.c
+++ keymaps/custom36/keymap.c
@@ -44,7 +44,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 			if (record->event.pressed){
 				tap_code(KC_ENT);
 				_delay_ms(100);
-				tap_code16(PSTE);
+				tap_code16(PSTE); //16비트로 복합키를 담을 수 있다.ㅣ
 				_delay_ms(100);
 				tap_code(KC_ENT);
 				_delay_ms(200);
@@ -107,11 +107,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	// Gaming
 	[_PUBG] = LAYOUT_split_3x6_3(
 		//,-----------------------------------------------------.                    ,-----------------------------------------------------.
-			 KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,   KC_P,TG(_PUBG),
+			 KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,  KC_ESC,TG(_PUBG),
 		//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 			KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_LNG1,
 		//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-			KC_LSFT,    KC_Z,    KC_X,  KC_LALT,    KC_V,   KC_H,                         KC_N,    KC_M, KC_COMM,  KC_DOT,  KC_ESC, KC_LGUI,
+			KC_LSFT,    KC_Z,    KC_X,  KC_LALT,    KC_V,   KC_H,                         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_LGUI, KC_LGUI,
 		//|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
 											       KC_C, KC_SPC,MO(_PSUB),	  SYM_ENT,NUM_BSPC, FUN_DEL
 											//`--------------------------'  `--------------------------'
@@ -141,7 +141,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	),
 	[_MEDIA] = LAYOUT_split_3x6_3(
 		//,-----------------------------------------------------.                    ,-----------------------------------------------------.
-			_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+			_______, KC_LGUI, XXXXXXX, XXXXXXX, KC_SLEP,  KC_PWR,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
 		//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
 			_______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, KC_MRWD, KC_VOLD, KC_VOLU, KC_MFFD, XXXXXXX,
 		//|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
